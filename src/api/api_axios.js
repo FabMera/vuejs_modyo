4518917926fd2c7a0397079c3a72db82f1e64c35
FabMera <fabianmeraromero@gmail.com>
2023-11-23 17:04:26 -0300

    se agregan las variables de entorno de modyo y se pushea el widgets a la plataforma,resultado ok

--- src/api/api_axios.js
+++ src/api/api_axios.js
@@ -8,3 +8,4 @@ export const api2 = axios.create({
     baseURL: `https://vass-chile.modyo.cloud/api/content/spaces/${process.env.VUE_APP_SPACE}/types/${process.env.VUE_APP_TYPE_PLAYLIST}/entries`
 })
 
+
